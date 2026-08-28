95f0bf6a8857b6a1d8abc717ef36dbdfad40a4ad
Anirudh Dwivedi <anirudhdwivedi1489@gmail.com>
2026-08-28 11:30:29 +0530

    Add version endpoint

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
     return {"message": "CI/CD practice application"}
 
+
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+@app.get("/version")
+def version():
+    return {"version": "1.1.0"}
